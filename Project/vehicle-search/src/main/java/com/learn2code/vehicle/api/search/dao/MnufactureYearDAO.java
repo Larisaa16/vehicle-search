12ee2f7deda416e432d5abf578974faef416310b
Larisaa16 <ciuciularisa@gmail.com>
2024-08-31 15:41:31 +0300

    created manufacturer and year and dao classes

---
+++ Project/vehicle-search/src/main/java/com/learn2code/vehicle/api/search/dao/MnufactureYearDAO.java
@@ -0,0 +1,8 @@
+package com.learn2code.vehicle.api.search.dao;
+
+import com.learn2code.vehicle.api.search.entity.ManufectureYear;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MnufactureYearDAO extends JpaRepository<ManufectureYear, Integer> {
+
+}
